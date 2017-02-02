1e7a5b7415575c76537350ae3806e9569476afd8
Brock Alberry <vowywowy@gmail.com>
2017-02-02 18:02:33 -0500

    router changes

--- src/app/app.js
+++ src/app/app.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
+	//add routes here
 	let routes = ['login'];
 	let head = document.querySelector('head');
 	routes.forEach(function (e) {
@@ -6,4 +7,29 @@ document.addEventListener('DOMContentLoaded', function () {
 		script.setAttribute('src', 'app/views/' + e + '/' + e + '.js');
 		head.appendChild(script);
 	});
+	//we load login as our home page
+	load(routes[0]);
 });
+
+//grabs the new view and replaces stale view
+function load(view) {
+	let request = new XMLHttpRequest();
+	request.open('GET', 'app/views/' + view + '/' + view + '.html', true);
+	request.send();
+	request.onreadystatechange = function () {
+		if (request.readyState === XMLHttpRequest.DONE && request.status === 200) {
+			document.getElementById('container').innerHTML = request.response;
+			let style = document.querySelectorAll('link');
+			style.forEach(function (e) {
+				if (!e.getAttribute('href').includes('base.css')) {
+					e.remove();
+				}
+			});
+			style = document.createElement('link');
+			style.setAttribute('rel', 'stylesheet');
+			style.setAttribute('href', 'app/views/' + view + '/' + view + '.css');
+			let head = document.querySelector('head');
+			head.appendChild(style);
+		}
+	};
+}
--- src/app/views/login/login.js
+++ src/app/views/login/login.js
@@ -1,8 +1,1 @@
-let request = new XMLHttpRequest();
-request.open('GET', 'app/views/login/login.html', true);
-request.send();
-request.onreadystatechange = function(){
-	if(request.readyState === XMLHttpRequest.DONE && request.status === 200){
-		document.getElementById('container').innerHTML = request.response;
-	}
-};
+console.log('Login page js accessed');
