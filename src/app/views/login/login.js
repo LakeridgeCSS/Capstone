e23fbd28b7ce24cb94d44065939b9921a9b9e885
Brock Alberry <vowywowy@gmail.com>
2017-02-02 17:34:15 -0500

    status fix

--- src/app/views/login/login.js
+++ src/app/views/login/login.js
@@ -2,5 +2,7 @@ let request = new XMLHttpRequest();
 request.open('GET', 'app/views/login/login.html', true);
 request.send();
 request.onreadystatechange = function(){
-	document.getElementById('container').innerHTML = request.response;
+	if(request.readyState === XMLHttpRequest.DONE && request.status === 200){
+		document.getElementById('container').innerHTML = request.response;
+	}
 };
